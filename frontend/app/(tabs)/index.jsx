6b755ad157797284a70ed54c84994314bc524fc5
Madison Ryan <maryan3@arizona.edu>
2026-03-24 11:52:19 -0400

    Changes made to get front end functioning and looking nice for final presentation in Nov or Dec 2024

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,39 +1,55 @@
 import {
 	FlatList,
+	Image,
 	Pressable,
 	SafeAreaView,
 	StyleSheet,
 	Text,
 	View,
 } from "react-native"
-import Header from "../../components/Header"
-import { useContext, useEffect } from "react"
+import { useCallback, useContext } from "react"
 import { UserContext } from "../../context/UserContext"
-import { Link, useRouter } from "expo-router"
+import { Link } from "expo-router"
 import Colors from "../../constants/Colors"
 import Styles from "../../constants/Styles"
 
 const recentVideos = [
-	{ title: "Yoga Video", uri: "", duration: "5:00" },
-	{ title: "Yoga Video", uri: "", duration: "5:00" },
+	{ title: "Flowsie Yoga Video", duration: "2 min", youtubeId: "giaFiF1glU4" },
 ]
 
 const recentFlows = [
-	{ title: "Temp Flow", uri: "" },
-	{ title: "Temp Flow", uri: "" },
+	{ title: "Temp Flow", sequence: [] },
+	{ title: "Temp Flow", sequence: [] },
 ]
 
 export default function HomeScreen() {
 	const { user } = useContext(UserContext)
+	console.log(user)
 
-	const renderClass = ({ lesson }) => (
-		<View style={styles.classCard}>
-			<Text>{lesson.date}</Text>
-			<Text>{lesson.time}</Text>
-			<Text>{lesson.title}</Text>
-			<Text>{lesson.instructor}</Text>
-		</View>
-	)
+	// date + "T" + time
+
+	const renderClass = useCallback((lesson) => {
+		const date = new Date(lesson.date + "T" + lesson.time + "-7:00")
+			.toDateString()
+			.split(" ")
+		return (
+			<View style={styles.classCard}>
+				<View style={{ alignContent: "center" }}>
+					<Text style={Styles.text}>{date[0]}</Text>
+					<Text style={{ ...Styles.subheader, color: Colors.ui }}>
+						{date[2]}
+					</Text>
+					<Text style={Styles.text}>{date[1]}</Text>
+				</View>
+				<View style={{ justifyContent: "center", gap: 5 }}>
+					<Text style={Styles.text}>
+						{lesson.title} with {lesson.instructor}
+					</Text>
+					<Text style={Styles.text}> {lesson.time}</Text>
+				</View>
+			</View>
+		)
+	})
 
 	const renderVideoCard = (video) => (
 		<Pressable
@@ -42,8 +58,13 @@ export default function HomeScreen() {
 			}}
 			style={styles.card}
 		>
-			<View style={styles.image}></View>
-			<Text style={styles.cardText}>{video?.title}</Text>
+			<Image
+				source={{
+					uri: `https://img.youtube.com/vi/${video.youtubeId}/default.jpg`,
+				}}
+				style={styles.videoImage}
+			/>
+			<Text style={styles.cardTitle}>{video?.title}</Text>
 		</Pressable>
 	)
 
@@ -52,20 +73,25 @@ export default function HomeScreen() {
 			onPress={() => {
 				// navigate to flow
 			}}
-			style={styles.card}
+			style={{ ...styles.card, flexDirection: "row", gap: 10 }}
 		>
-			<View style={styles.image}></View>
-			<Text style={styles.cardText}>{flow?.title}</Text>
+			<View style={{ justifyContent: "center" }}>
+				<Text style={styles.cardTitle}>{flow?.title}</Text>
+				<Text style={styles.cardText}>{flow?.difficulty}</Text>
+			</View>
+			<View
+				style={{ padding: 2.5, borderLeftWidth: 1, borderColor: Colors.border }}
+			>
+				<Image source={{uri: "https://drive.google.come/file/d/"+flow.sequence[0]?.image+"/preview"}} style={styles.flowImage} />
+			</View>
 		</Pressable>
 	)
-
-	console.log(user)
 
 	return (
 		<SafeAreaView style={Styles.container}>
 			<View style={styles.greetingView}>
-				<Text style={{...Styles.header, color: Colors.ui}}>
-					{"Hello, " + (user?.name ? user.name : "Guest")}
+				<Text style={{ ...Styles.header, color: Colors.ui }}>
+					{"Hello, " + (user?.name ? user.name.split(" ")[0] : "Guest")}
 				</Text>
 			</View>
 			{user === null && (
@@ -76,25 +102,28 @@ export default function HomeScreen() {
 				</View>
 			)}
 			{user !== null && (
-				<View style={{ gap: 10 }}>
-					<View>
+				<View style={{ flex: 1, marginBottom: 15, gap: 20 }}>
+					<View style={{ flex: 1 }}>
 						<Text style={styles.sectionTitle}>Upcoming Classes</Text>
-						<View style={styles.listView}>
+						<View style={styles.classView}>
 							<FlatList
 								data={user.classes}
-								renderItem={renderClass}
+								renderItem={({ item }) => renderClass(item)}
 								extraData={user}
 								ListEmptyComponent={() => (
-									<Link style={Styles.text} href="/calendar">
-										No Classes
+									<Link
+										style={{ ...Styles.text, color: Colors.ui }}
+										href="/calendar"
+									>
+										Sign up for yoga classes here!
 									</Link>
 								)}
 							/>
 						</View>
 					</View>
 					<View>
-						<Text style={styles.sectionTitle}>Recent Videos</Text>
-						<View style={styles.listView}>
+						<Text style={styles.sectionTitle}>Recently Watched</Text>
+						<View>
 							<FlatList
 								data={recentVideos}
 								renderItem={({ item }) => renderVideoCard(item)}
@@ -102,8 +131,11 @@ export default function HomeScreen() {
 								contentContainerStyle={{ gap: 10 }}
 								extraData={user}
 								ListEmptyComponent={() => (
-									<Link style={Styles.text} href="/videos">
-										No Videos
+									<Link
+										style={{ ...Styles.text, color: Colors.ui }}
+										href="/videos"
+									>
+										Watch a yoga video!
 									</Link>
 								)}
 							/>
@@ -111,16 +143,19 @@ export default function HomeScreen() {
 					</View>
 					<View>
 						<Text style={styles.sectionTitle}>Saved Flows</Text>
-						<View style={styles.listView}>
+						<View>
 							<FlatList
-								data={recentFlows}
+								data={user.flows}
 								renderItem={({ item }) => renderFlowCard(item)}
 								horizontal
 								contentContainerStyle={{ gap: 10 }}
 								extraData={user}
 								ListEmptyComponent={() => (
-									<Link style={Styles.text} href="/flows">
-										No Flows
+									<Link
+										style={{ ...Styles.text, color: Colors.ui }}
+										href="/flows"
+									>
+										Try making a flow!
 									</Link>
 								)}
 							/>
@@ -136,21 +171,26 @@ const styles = StyleSheet.create({
 	greetingView: { marginTop: 10 },
 	classView: {
 		backgroundColor: Colors.tintedBackground,
-		padding: 5,
+		padding: 8,
+		flex: 1,
+		borderWidth: 1,
+		borderRadius: 10,
+		borderColor: Colors.border,
 	},
 	classCard: {
 		backgroundColor: Colors.background,
-		padding: 5,
+		flexDirection: "row",
+		padding: 10,
+		paddingHorizontal: 20,
+		gap: 15,
+		borderWidth: 1,
+		borderRadius: 10,
+		borderColor: Colors.border,
 	},
 	sectionTitle: {
 		...Styles.subheader,
+		marginBottom: 5,
 		borderBottomWidth: 2,
-	},
-	listView: {
-		backgroundColor: Colors.tintedBackground,
-		padding: 10,
-		borderWidth: 1,
-		borderRadius: 10,
 		borderColor: Colors.border,
 	},
 	list: {
@@ -163,13 +203,27 @@ const styles = StyleSheet.create({
 		borderRadius: 10,
 		borderColor: Colors.border,
 	},
+	cardTitle: {
+		...Styles.subheader,
+		fontSize: 16,
+		padding: 10,
+	},
 	cardText: {
 		...Styles.text,
 		padding: 10,
+		paddingTop: 0,
+		color: Colors.inactiveUI,
 	},
-	image: {
-		height: 80,
-		aspectRatio: "4/3",
+	videoImage: {
+		height: 100,
+		aspectRatio: "16/9",
 		backgroundColor: Colors.tintedBackground,
 	},
+	flowImage: {
+		height: 75,
+		width: 75,
+		resizeMode: "contain",
+	},
 })
+
+// want images to overlap w/ text box
